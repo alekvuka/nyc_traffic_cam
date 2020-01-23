44e56765e0e4d1181e4fa00cb8d6951f666f6105
github username <alekvuka@gmail.com>
2020-01-23 16:52:01 -0500

    add redux imports to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import manageCameras from './reducers/manageCameras.js'
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+
+ReactDOM.render(
+
+  <Provider store={store}>
+  <App />
+</Provider>,
+
+
+  document.getElementById('root')
+
+
+);
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
