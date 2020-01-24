272ef90b9057f00f3f2d64db8a5c5d0a49764a6c
github username <alekvuka@gmail.com>
2020-01-23 22:51:11 -0500

    create cameras, cameras/:id and avenues routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'cameras', to: 'cameras#all'
+  get 'cameras/:id' to: 'cameras#show'
+  get 'avenues', to: 'avenues#all'
+
+
 end
