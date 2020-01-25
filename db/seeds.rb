02f14a5364a14ace9b1f4a28c19b086b6c33119f
github username <alekvuka@gmail.com>
2020-01-25 01:40:22 -0500

    add initial avenue and camera seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+avenues = Avenue.create([
+  {name: "First Avenue"},
+  {name: "Second Avenue"},
+  {name: "Third Avenue"},
+  {name: "Lexington Avenue"},
+  {name: "FDR"}
+  ])
+
+Camera.create([
+  {description: "1st av. at 110th st", url:"http://207.251.86.238/cctv261.jpg?rand=", avenue: avenues.first},
+  {description: "1st av. at 14th st", url:"http://207.251.86.238/cctv1083.jpg?rand=", avenue: avenues.first}
+  ])
