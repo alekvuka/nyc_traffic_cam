86a0673edf39d8ac961c7fbb2fb32e906ab3863c
github username <alekvuka@gmail.com>
2020-01-26 15:59:08 -0500

    add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import HeaderContainer from './containers/HeaderContainer.js'
 import CamerasContainer from './containers/CamerasContainer.js'
 import Container from 'react-bootstrap/Container'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
+
         <Container fluid>
             <HeaderContainer />
             <CamerasContainer />
         </Container>
+    
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import ManageCameras from './reducers/ManageCameras.js'
 
