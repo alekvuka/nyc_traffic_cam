87524b7671b241ae926c79eb772dadc3b7d9bb53
github username <alekvuka@gmail.com>
2020-01-27 16:59:51 -0500

    remove Router from index/js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import ManageCameras from './reducers/ManageCameras.js'
 
