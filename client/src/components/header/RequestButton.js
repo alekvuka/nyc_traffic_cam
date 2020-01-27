d675d56561ed414571a5a4e7103921b149dd3f6f
github username <alekvuka@gmail.com>
2020-01-27 16:50:39 -0500

    remove FormControl from RequestButton

--- client/src/components/header/RequestButton.js
+++ client/src/components/header/RequestButton.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Spinner from 'react-bootstrap/Spinner'
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
 import { NavLink } from 'react-router-dom';
 
 const RequestButton = props => {
